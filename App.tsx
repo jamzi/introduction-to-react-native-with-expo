6dbdf121bbc5605a5de8e38fad3dda30aa94ecee
Janez <janez.cadez007@gmail.com>
2023-10-05 21:41:18 +0200

    show alert and button

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Alert, Button } from "react-native";
 
 export default function App() {
+  const handleButtonPress = () => {
+    Alert.alert("Hey there");
+  };
+
   return (
     <View style={styles.container}>
       <Text>Introduction to React Native with Expo</Text>
+      <Button onPress={handleButtonPress} title="Open Dialog" />
       <StatusBar style="auto" />
     </View>
   );
