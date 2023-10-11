7825eec140215bfab53c6b8a3a7e42c16fb53485
Janez <janez.cadez007@gmail.com>
2023-10-11 09:47:04 +0200

    go back implementation

--- src/DetailsScreen.tsx
+++ src/DetailsScreen.tsx
@@ -1,12 +1,23 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import { DetailsScreenProps } from "./navigation/types";
 
-export default function DetailsScreen({ route }: DetailsScreenProps) {
+export default function DetailsScreen({
+  navigation,
+  route,
+}: DetailsScreenProps) {
   const email = route.params?.email;
+
+  const handleGoBack = () => {
+    if (navigation.canGoBack()) {
+      navigation.goBack();
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Details Screen</Text>
       <Text>{email}</Text>
+      <Button title="Go Back" onPress={handleGoBack} />
     </View>
   );
 }
