dd415eab103b276dcba605ca525f5a2582c5215f
Janez <janez.cadez007@gmail.com>
2023-10-17 19:51:59 +0200

    added styles + safe area insets

--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -1,14 +1,17 @@
 import { StyleSheet, View, Text, Button } from "react-native";
 import { HomeScreenProps } from "./navigation/types";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function HomeScreen({ navigation }: HomeScreenProps) {
+  const insets = useSafeAreaInsets();
+
   const navigateToDetails = () => {
     navigation.navigate("Details", { email: "test@test.com" });
   };
 
   return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
+    <View style={[styles.container, { paddingTop: insets.top }]}>
+      <Text style={styles.text}>Home Screen</Text>
       <Button title="Navigate to Details" onPress={navigateToDetails} />
     </View>
   );
@@ -17,8 +20,8 @@ export default function HomeScreen({ navigation }: HomeScreenProps) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "lightblue",
+    paddingHorizontal: 20,
   },
+  text: { fontSize: 20 },
 });
