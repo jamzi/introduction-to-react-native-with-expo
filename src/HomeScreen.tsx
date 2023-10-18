5fabe00ae6395371c577a785b6b1c39e62343704
Janez <janez.cadez007@gmail.com>
2023-10-18 20:08:30 +0200

    add two external packages, add code for share and device info

--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -1,6 +1,8 @@
 import { StyleSheet, View, Text, Button } from "react-native";
 import { HomeScreenProps } from "./navigation/types";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import * as Device from "expo-device";
+import Share from "react-native-share";
 
 export default function HomeScreen({ navigation }: HomeScreenProps) {
   const insets = useSafeAreaInsets();
@@ -9,10 +11,26 @@ export default function HomeScreen({ navigation }: HomeScreenProps) {
     navigation.navigate("Details", { email: "test@test.com" });
   };
 
+  const handleShare = async () => {
+    try {
+      const shareOptions = {
+        title: "Share file",
+        message: "Simple share with message",
+        url: "https://google.com",
+      };
+      await Share.open(shareOptions);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <View style={[styles.container, { paddingTop: insets.top }]}>
       <Text style={styles.text}>Home Screen</Text>
+      <Text style={styles.text}>{Device.brand}</Text>
+      <Text style={styles.text}>{`${Device.osName} ${Device.osVersion}`}</Text>
       <Button title="Navigate to Details" onPress={navigateToDetails} />
+      <Button title="Share" onPress={handleShare} />
     </View>
   );
 }
